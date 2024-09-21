831859e911bfcf3df51d392e25535c6ba9a452e0
Naveena-V-18 <Naveenavelumani5@gmail.com>
2024-09-21 18:22:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Form submission alert
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("Thank you for contacting us! We'll get back to you soon.");
+});
